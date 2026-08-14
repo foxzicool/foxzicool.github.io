b67ad49d294619ca44229e0ae65ad23d9bb6c52e
Terry Chang <rwerwerwerwtt@gmail.com>
2026-08-15 04:03:23 +0800

    erwe174424242

--- api/data.js
+++ api/data.js
@@ -1,5 +1,28 @@
 export default function handler(req, res) {
 
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://foxzicool.github.io"
+    );
+
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    );
+
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type"
+    );
+
+
+    if (req.method === "OPTIONS") {
+
+        return res.status(204).end();
+
+    }
+
+
     res.status(200).json({
 
         success: true,
